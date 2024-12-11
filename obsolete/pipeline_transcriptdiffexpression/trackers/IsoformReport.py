71a1639ce15e95bb13fb5f34bb6aee43e2968230
jscaber <jakub.scaber@ndcn.ox.ac.uk>
2024-12-11 22:57:49 +0000

    remove obsolete

--- obsolete/pipeline_transcriptdiffexpression/trackers/IsoformReport.py
+++
@@ -1,5 +0,0 @@
-from cgatReport.Tracker import TrackerSQL
-
-
-class IsoformTracker(TrackerSQL):
-    """Base class for trackers in this report"""
--- obsolete/reports/pipeline_peakcalling_obsolete/trackers/Mapping.py
+++
@@ -1,6 +0,0 @@
-from PeakcallingReport import *
-from cgatReport.Tracker import *
-
-
-class BamSummary(CallingTracker, SingleTableTrackerRows):
-    table = "bam_stats"
